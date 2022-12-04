6c00d4b6ccbaee435874bd338830c275d861346f
Akshaypote8888 <akshaypote08@gmail.com>
2022-12-04 18:11:19 +0530

    done

---
+++ test-04-12-22/script.js
@@ -0,0 +1,21 @@
+var clickDot1 = document.getElementById('dot1')
+var clickDot2 = document.getElementById('dot2')
+var clickDot3 = document.getElementById('dot3')
+var clickDot4 = document.getElementById('dot4')
+var page = document.getElementById('main_container')
+
+function chngegray() {
+    document.body.style.backgroundColor = "gray";
+}
+function chngewhite() {
+    document.body.style.backgroundColor = "white";
+}
+function chngeblue() {
+    document.body.style.backgroundColor = "blue";
+}
+function chngeyellow() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+
+
