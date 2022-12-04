566b1a43a64412d7cdf76331166b8392dcf80697
Akshaypote8888 <akshaypote08@gmail.com>
2022-12-04 19:14:21 +0530

    done

---
+++ test-04-12-22/Q-2/script.js
@@ -0,0 +1,25 @@
+let i = 3
+document.getElementById('dec_button').onclick = function decrease() {
+    i -= 1;
+    if(i<0){
+        document.getElementById('count').style.color = 'red';
+    }
+    document.getElementById('count').innerHTML = i;
+}
+
+document.getElementById('res_button').onclick = function reset() {
+    i = 3;
+    document.getElementById('count').style.color = 'green';
+    document.getElementById('count').innerHTML = i;
+}
+
+document.getElementById('inc_button').onclick = function increase() {
+    i += 1;
+    if(i>0){
+        document.getElementById('count').style.color = 'green';
+    }
+    document.getElementById('count').innerHTML = i;
+}
+
+
+
