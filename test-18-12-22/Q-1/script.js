babc285bde08ac0700326c84a86f4c63de42913e
Akshaypote8888 <akshaypote08@gmail.com>
2022-12-25 12:23:27 +0530

    done

--- test-18-12-22/Q-1/script.js
+++ test-18-12-22/Q-1/script.js
@@ -1,6 +1,6 @@
 const country = document.getElementById('country')
 const stateInput = document.getElementById('state')
-const containerFull = document.getElementById('container')
+const stateNameblock = document.getElementById('state_name')
 const universityName = document.getElementById('university_name')
 const siteUrl = document.getElementById('site')
 const searchButton = document.getElementById('search_button')
@@ -24,19 +24,10 @@ async function fetchUniversity(){
 }
 function renderUI(jsonData){
     const stateName = stateInput.value;
-    console.log(stateName)
     for(let i=0;i<jsonData.length;i++){
         if(jsonData[i]['state-province']== 'stateName'){
-            containerFull.innerHTML = `<div id="cards">
-            <h4>State Name : - <span id="state_name">${jsonData[i]['state-province']}</span></h4>
-            <br>
-            <h4>University Name : - <span id="university_name">Pune University</span></h4>
-            <br>
-            <h4>University site URL : - <a id="site" href="www.google.com">www.google.com</a> </span></h4>
-            <br>
-        </div>`;
+            stateNameblock.innerHTML = stateName;
         }
         break;
     }
-    console.log(containerFull)
 }
